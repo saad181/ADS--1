5d7eda084caf7ef33f95475bf60ea9f216f8cfc3
saad181 <saadahmed181@msitprogram.net>
2018-09-26 23:10:45 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- assignment/m3/3-sum/Solution.java
+++ assignment/m3/3-sum/Solution.java
@@ -1,41 +1,44 @@
 import java.util.Scanner;
 import java.util.Arrays;
+/**
+ *  class.
+ **/
 public class Solution {
-	/**
-	 *  args.
-	 *
-	 * @param      args  The arguments
-	 **/
-	public static void main(String[] args) {
-	/**
-	 * 
-	 *{ var_description }
-	 **/
-		Scanner scan = new Scanner(System.in);
-		int arraysize = scan.nextInt();
-		int[] array = new int[arraysize];
-		int count = 0;
-		for (int i = 0; i < arraysize; i++) {
-			array[i] = scan.nextInt();
-		}
-		Arrays.sort(array);
-		int j = 0;
-		int k = 0;
-		for (int i = 0; i < arraysize; i++) {
-			j = i + 1;
-			k = arraysize - 1;
-			while (j < k) {
-				if (array[i] + array[j] + array[k] < 0) {
-					j += 1;
-				} else if (array[i] + array[j] + array[k] > 0) {
-					k -= 1;
-				} else {
-					j += 1;
-					k -= 1;
-					count += 1;
-				}
-			}
-		}
-		System.out.println(count);
-	}
+    /**
+     *  args.
+     *
+     * @param      args  The arguments
+     **/
+    final public static void main(String[] args) {
+    /**
+     * 
+     *{ var_description }
+     **/
+        Scanner scan = new Scanner(System.in);
+        int arraysize = scan.nextInt();
+        int[] array = new int[arraysize];
+        int count = 0;
+        for (int i = 0; i < arraysize; i++) {
+            array[i] = scan.nextInt();
+        }
+        Arrays.sort(array);
+        int j = 0;
+        int k = 0;
+        for (int i = 0; i < arraysize; i++) {
+            j = i + 1;
+            k = arraysize - 1;
+            while (j < k) {
+                if (array[i] + array[j] + array[k] < 0) {
+                    j += 1;
+                } else if (array[i] + array[j] + array[k] > 0) {
+                    k -= 1;
+                } else {
+                    j += 1;
+                    k -= 1;
+                    count += 1;
+                }
+            }
+        }
+        System.out.println(count);
+    }
 }
