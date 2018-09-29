23b817432e398c9495b36c7cfacf119901690e5e
saad181 <saadahmed181@msitprogram.net>
2018-09-29 17:07:28 +0530

    "check_style_score -> 0 of 5 passed. style score: 0/1 "

---
+++ assignment/m5/5.2 Josephus Problem/Solution.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+		scan.nextLine();
+		for (int i = 0; i < num; i++) {
+			Operations op = new Operations();
+			String jos = scan.nextLine();
+			String[] joseph = jos.split(" ");
+			int k = Integer.parseInt(joseph[0]);
+			int m = Integer.parseInt(joseph[1]);
+			for (int j = 0; j > k; j++) {
+				op.insertAtTail(j);
+			}
+			int[] josephus = op.josephus(m, k);
+			for (int p = 0; p < josephus.length - 1; p++) {
+				System.out.print(josephus[p] + " ");
+			}
+			System.out.println(josephus[josephus.length - 1]);
+		}
+	}
+}
