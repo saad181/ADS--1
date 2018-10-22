ea2ea3e257cd60a9bf40d8f48e0a09e6142b76d6
saad181 <saadahmed181@msitprogram.net>
2018-10-22 21:35:18 +0530

    "ads1_m19_a1: 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ assignment/m19/19.1 BST/BST.java
@@ -0,0 +1,287 @@
+public class BST<Key extends Comparable<Key>, Value> {
+    private Node root;
+    int size = 0;
+    /**
+     * Class for node.
+     */
+    private class Node {
+        /**
+         * created a variable key.
+         */
+        private Key key;
+        /**
+         * created a variable value.
+         */
+        private Value value;
+        /**
+         * created a variable left.
+         */
+        private Node left;
+        /**
+         * created a variable right.
+         */
+        private Node right;
+        /**
+         * created a variable count.
+         */
+        private int count;
+    }
+    /**
+     * created a  method put.
+     *
+     * @param      key    The key
+     * @param      value  The value
+     */
+    public void put(Key key, Value value) {
+        if (key == null)  {
+            System.out.println("null");
+        }
+        root = put(root, key, value);
+    }
+    /**
+     * the time complexity for the method is O(logN).
+     *
+     * @param      root   The root
+     * @param      key    The key
+     * @param      value  The value
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private Node put(Node root, Key key, Value value) {
+        if (root == null) {
+            Node n = new Node();
+            n.key = key;
+            n.value = value;
+            n.left = null;
+            n.right = null;
+            root = n;
+            size++;
+        }
+        int cmp = key.compareTo(root.key);
+        if(cmp < 0) {
+            root.left  = put(root.left,  key, value);
+        }
+        else if (cmp > 0) {
+            root.right = put(root.right, key, value);
+        }
+        else {
+            root.value = value;
+        }
+        root.count = 1 + size(root.left) + size(root.right);
+        return root;
+
+    }
+    public Value get(Key key) {
+        return get(root, key);
+    }
+    /**
+     * the time complexity for the method is O(logN).
+     *
+     * @param      x     { parameter_description }
+     * @param      key   The key
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private Value get(Node x, Key key) {
+        if (key == null) {
+            System.out.println("empty");
+        }
+        if (x == null) {
+            //System.out.println("in get");
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            return get(x.left, key);
+        }
+        else if (cmp > 0) {
+            return get(x.right, key);
+        }
+        else {
+            return x.value;
+        }
+    }
+    /**
+     * created a method min.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Key min() {
+        return min(root).key;
+    }
+    /**
+     * created a method min.
+     *
+     * @param      x     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private Node min(Node x) {
+        if (x.left == null) {
+            return x; 
+        }
+        else {
+            return min(x.left); 
+        }
+    }
+    /**
+     * created a method max.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Key max() {
+        return max(root).key;
+    }
+    /**
+     * created a method max.
+     *
+     * @param      x     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private Node max(Node x) {
+        if (x.right == null) {
+            return x;
+        }
+        else {
+            return max(x.right);
+        }
+    }
+    /**
+     * created a method floor.
+     *
+     * @param      key   The key
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Key floor(Key key) {
+        Node x = floor(root, key);
+        if (x == null) {
+            return null;
+        }
+        return x.key;
+    }
+    /**
+     * created a method floor.
+     *
+     * @param      x     { parameter_description }
+     * @param      key   The key
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private Node floor(Node x, Key key) {
+         if (x == null) {
+            return null;
+         }
+         int cmp = key.compareTo(x.key);
+         if (cmp == 0) {
+            return x;
+         }
+         if (cmp < 0) {
+            return floor(x.left, key);            
+         }
+         Node t = floor(x.right, key);
+         if (t != null) {
+            return t;
+         }
+         else {
+            return x;
+         }
+    }
+    /**
+     * created a method ceiling.
+     *
+     * @param      key   The key
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Key ceiling(Key key) {
+        Node x = ceiling(root, key);
+        if (x == null) {
+            return null;
+        }
+        return x.key;
+    }
+    /**
+     * created a method ceiling.
+     *
+     * @param      x     { parameter_description }
+     * @param      key   The key
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private Node ceiling(Node x, Key key) {
+        if (x == null) {
+            return null;
+         }
+         int cmp = key.compareTo(x.key);
+         if (cmp == 0) {
+            return x;
+         }
+         if (cmp > 0) {
+            return ceiling(x.right, key);            
+         }
+         Node t = ceiling(x.left, key);
+         if (t != null) {
+            return t;
+         }
+         else {
+            return x;
+         }
+    }
+    /**
+     * created a method size.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int size() { 
+        return size(root);
+    }
+    /**
+     * created a method size.
+     *
+     * @param      x     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private int size(Node x) {
+        if (x == null) {
+            return 0;
+        }
+        return x.count;
+    }
+    /**
+     * created a method size.
+     *
+     * @param      k     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Key select(int k) {
+        Node x = select(root, k);
+        return x.key;
+    }
+    /**
+     * created a method select.
+     *
+     * @param      x     { parameter_description }
+     * @param      k     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private Node select(Node x, int k) {
+        if (x == null) {
+            return null; 
+        }
+        int t = size(x.left); 
+        if (t > k) {
+            return select(x.left,  k); 
+        }
+        else if (t < k) {
+            return select(x.right, k - t - 1); 
+        }
+        else {
+            return x; 
+        }
+    }
+}
---
+++ assignment/m19/19.1 BST/Solution.java
@@ -0,0 +1,146 @@
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() { }
+    /**
+     * { function_description }.
+     * the time complexity for the main method is O(n).
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        BST<Book, String> bst = new BST<Book, String>();
+        while (scan.hasNext()) {
+            String[] tokens = scan.nextLine().split(",");
+            switch (tokens[0]) {
+            case "put":
+                bst.put(new Book(tokens[1], tokens[2], tokens[2 + 1]),
+                    tokens[2 + 2]);
+                break;
+            case "get":
+                System.out.println(
+                    bst.get(new Book(tokens[1], tokens[2], tokens[2 + 1])));
+                break;
+            case "max":
+                System.out.println(bst.max().toString());
+                break;
+            case "min":
+                System.out.println(bst.min().toString());
+                break;
+            case "select":
+                System.out.println(bst.select(
+                    Integer.parseInt(tokens[1])).toString());
+                break;
+            case "floor":
+                System.out.println(bst.floor(
+                    new Book(tokens[1], tokens[2], tokens[2 + 1])).toString());
+                break;
+            case "ceiling":
+                System.out.println(bst.ceiling(
+                    new Book(tokens[1], tokens[2], tokens[2 + 1])).toString());
+                break;
+            default:
+                System.out.println("default call");
+            }
+        }
+    }
+}
+
+/**
+ * Class for book.
+ */
+class Book implements Comparable<Book> {
+    /**
+     * { var_description.
+     */
+    private String name;
+    /**
+     * { var_description.
+     */
+    private String author;
+    /**
+     * var_description.
+     */
+    private Double price;
+    /**
+     * Constructs the object.
+     */
+    private Book() { }
+    /**
+     * Constructs the object.
+     *
+     * @param      bookname    The bname
+     * @param      bookauthor  The bauthor
+     * @param      bookprice   The bprice
+     */
+    Book(final String bookname, final String bookauthor,
+        final String bookprice) {
+        this.name = bookname;
+        this.author = bookauthor;
+        this.price = Double.parseDouble(bookprice);
+    }
+    /**
+     * function_description.
+     *
+     * @return     { description_of_the_return_value }
+     */
+     public String getname() {
+        return this.name;
+    }
+    /**
+     * function_description.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public String getauthor() {
+        return this.author;
+    }
+    /**
+     * function_description.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Double getprice() {
+        return this.price;
+    }
+    /**
+     * function_description.
+     *
+     * @param      that  The that
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int compareTo(final Book that) {
+        if (this.getname().compareTo(that.getname()) > 0) {
+            return 1;
+        } else if (this.getname().compareTo(that.getname()) < 0) {
+            return -1;
+        } else if (this.getauthor().compareTo(that.getauthor()) < 0) {
+            return -1;
+        } else if (this.getauthor().compareTo(that.getauthor()) > 0) {
+            return 1;
+        }  else if (this.getprice() > that.getprice()) {
+            return 1;
+        } else if (this.getprice() < that.getprice()) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+}
+//     /**
+//      * Returns a string representation of the object.
+//      *
+//      * @return     String representation of the object.
+//      */
+//     public String toString() {
+//         String m = "";
+//         m += this.getname() + ", " + this.getauthor() + ", " + this.getprice();
+//         return m;
+//     }
+// }
